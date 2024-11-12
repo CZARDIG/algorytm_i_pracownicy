36f26f0eaaf36eb4f09622d956e51f411f5cf9e7
CZARDIG <67341998+CZARDIG@users.noreply.github.com>
2024-11-12 11:21:31 +0100

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0; 
+            int b = 0;
+
+            Console.WriteLine("Wpisz liczbe a: ");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Wpisz liczbe b: ");
+            int.TryParse(Console.ReadLine(), out b);
+
+            if(a > 0 && b > 0)
+            {
+                while(a != b)
+                {
+                        if (a > b)
+                        {
+                            a = a - b;
+                        }
+                        else
+                        {
+                            b = b - a;
+                        }
+                    }
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("Podałeś niepoprawną wartość");
+            }
+        }
+    }
+}
